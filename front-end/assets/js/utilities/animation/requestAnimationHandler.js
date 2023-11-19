e71811e1adb95b85625695601193f844bb1d66e2
anastar5858 <anas.tarrab58@gmail.com>
2023-11-19 00:22:20 +0000

    animation integrated (move to mobile design and editing status and deleting a request only owner)

---
+++ front-end/assets/js/utilities/animation/requestAnimationHandler.js
@@ -0,0 +1,134 @@
+const animationHanlder = (mode, cardContainer, setIndexCounter, lengthToCompare, index) => {
+    // only translation is applied
+    if (mode === 'default') {
+        cardContainer.current.style.animation = 'translateY 3s 1';
+        cardContainer.current.addEventListener('animationend', () => {
+            cardContainer.current.style.animation = 'none';
+            if (lengthToCompare === 0) {
+                setIndexCounter(index > 0 ? index - 1 : index)
+            } else {
+                setIndexCounter(index < lengthToCompare ? index + 1 : index)    
+            }
+        });
+        console.log(mode, cardContainer, setIndexCounter, lengthToCompare);
+    }
+    // rocket animation
+    if (mode === 'rocket') {
+        // make all content invisible
+        hideElements();
+        // add the counter header
+        const counter = document.createElement('h1');
+        counter.id = 'rocketCount';
+        counter.textContent = '1';
+        cardContainer.current.appendChild(counter);
+        // make the card container position relative
+        cardContainer.current.style.position = 'relative';
+        // add the clip path
+        document.getElementById('ellipse-rocket').style.clipPath = 'polygon(50% 18%, 100% 15%, 100% 100%, 0 100%, 0 15%)';
+        let count = 0;
+        let degrees = -10
+        const prepareRocketLaunch = () => {
+            document.getElementById('rocketCount').textContent = count + 1
+            const cardContainer = document.getElementById('cardContainer');
+            if (count === 9) {
+                count = 0;
+                degrees = -10;
+                // show the cloud and translate
+                const cloud = document.createElement('div');
+                cloud.classList.add('fireCloud');
+                cardContainer.appendChild(cloud);
+                setTimeout(() => {
+                    const cardContainer =  document.getElementById('cardContainer')
+                    cardContainer.style.animation = 'translateY 3s 1 forwards';
+                    cardContainer.addEventListener('animationend', () => {
+                        // revert all added configurations
+                        unhideElements()
+                        const countHeader = document.getElementById('rocketCount');
+                        if (countHeader) countHeader.remove();
+                        cardContainer.style.position = 'static';
+                        document.getElementById('ellipse-rocket').style.clipPath = 'none';
+                        const lines = [...document.getElementsByClassName('line-container')];
+                        lines.forEach((line) => line.remove());
+                        const clouds = [...document.getElementsByClassName('fireCloud')];
+                        clouds.forEach((cloud) => cloud.remove());
+                        cardContainer.style.animation = 'none';
+                        if (lengthToCompare === 0) {
+                            setIndexCounter(index > 0 ? index - 1 : index)
+                        } else {
+                            setIndexCounter(index < lengthToCompare ? index + 1 : index)    
+                        }
+                    });
+                }  , 2000)
+                return
+            } else {
+                const newLine = document.createElement('div');
+                newLine.classList.add('line-container');
+                newLine.style.transform = `rotate(${degrees}deg)`;
+                count++
+                degrees -= 20;
+                cardContainer.appendChild(newLine)
+                setTimeout(() => requestAnimationFrame(prepareRocketLaunch), 500)    
+            }
+        }
+        requestAnimationFrame(prepareRocketLaunch);
+    }
+    // the fly animation
+    if (mode === 'fly') {
+        hideElements();
+        const style = document.createElement('style');
+        document.head.appendChild(style);
+        const styleSheet = style.sheet;
+        styleSheet.insertRule(`
+        #ellipse::before {
+            visibility: visible;
+          }
+        `, styleSheet.cssRules.length);
+        
+        styleSheet.insertRule(`
+          #ellipse::after {
+            visibility: visible;
+          }
+        `, styleSheet.cssRules.length);
+        
+        setTimeout(() => {
+        const cardContainer =  document.getElementById('cardContainer')
+        cardContainer.style.animation = 'translateY 3s 1'; 
+        cardContainer.addEventListener('animationend', () => {
+            unhideElements();
+            cardContainer.style.animation = 'none'; 
+            styleSheet.insertRule(`
+            #ellipse::before {
+                visibility: hidden;
+              }
+            `, styleSheet.cssRules.length);
+            
+            styleSheet.insertRule(`
+              #ellipse::after {
+                visibility: hidden;
+              }
+            `, styleSheet.cssRules.length);
+            if (lengthToCompare === 0) {
+                setIndexCounter(index > 0 ? index - 1 : index)
+            } else {
+                setIndexCounter(index < lengthToCompare ? index + 1 : index)    
+            }
+        }); 
+        }, 2000);  
+    }
+}
+
+const hideElements = () => {
+    document.getElementById('request-title').style.visibility = 'hidden';
+    document.getElementById('request-desc').style.visibility = 'hidden';
+    document.getElementById('request-status').style.visibility = 'hidden';
+    document.getElementById('creator-sign').style.visibility = 'hidden';
+    document.getElementById('date-sign').style.visibility = 'hidden';
+}
+
+const unhideElements = () => {
+    document.getElementById('request-title').style.visibility = 'visible';
+    document.getElementById('request-desc').style.visibility = 'visible';
+    document.getElementById('request-status').style.visibility = 'visible';
+    document.getElementById('creator-sign').style.visibility = 'visible';
+    document.getElementById('date-sign').style.visibility = 'visible';
+}
--- front-end/view/dashboard/dashboard-components/manageView.js
+++ front-end/view/dashboard/dashboard-components/manageView.js
@@ -1,6 +1,9 @@
 const ManageView = () => {
+    const cardContainerRef = React.useRef(null);
     const [request, setRequests] = React.useState([]);
-    const [testSample, setTestSample] = React.useState({});
+    const [cardMode, setCardMode] = React.useState('default');
+    const [indexCounter, setIndexCounter] = React.useState(0);
+    const [currentRequest, setCurrentRequest] = React.useState({});
     React.useEffect(() => {
         const fetchAllRecords = async () => {
             const allRequestsRequest = await fetch('http://localhost:8080/api/all-requests', {
@@ -10,16 +13,73 @@ const ManageView = () => {
             if (allRequestsRequest.ok) {
                 const allRequestsResponse = await allRequestsRequest.json();
                 setRequests(allRequestsResponse);
-                setTestSample(allRequestsResponse[0]);
+                setCurrentRequest(allRequestsResponse[indexCounter]);
             } else {
                 // todo: handle server error
             }
         }
         fetchAllRecords()
     }, [])
+    React.useEffect(() => {
+        if (request.length > 0) {
+            setCurrentRequest(request[indexCounter]);
+        }
+    }, [indexCounter])
   return (
     <>
-    <h1>Hello World!</h1>
+    {/* navigation system */}
+    <div id='.flex-column'>
+        <label className="radio-container label" htmlFor='prev'>
+            <strong>Previous</strong>
+            <input onClick={() => {
+                animationHanlder(cardMode, cardContainerRef, setIndexCounter, 0, indexCounter)
+            }}  id='prev' type='radio' value='prev' name='navigation'></input>
+            <span className="radio-dot"></span>
+        </label>
+        <label className="radio-container label" htmlFor='next'>
+            <strong>Next</strong>
+            <input onClick={() => {
+                animationHanlder(cardMode, cardContainerRef, setIndexCounter, request.length - 1, indexCounter)
+            }}  id='next' type='radio' value='next' name='navigation'></input>
+            <span className="radio-dot"></span>                 
+        </label>  
+    </div>
+    {/* animation mode switchers */}
+    <label onClick={() => setCardMode('default')} className="radio-container label" htmlFor='default'>
+        <strong>No Animation</strong>
+        <input id='default' type='radio' value='default' name='animation'></input>
+        <span className="radio-dot"></span>
+    </label>
+    <label onClick={() => setCardMode('fly')} className="radio-container label" htmlFor='fly'>
+        <strong>Fly Animation</strong>
+        <input id='fly' type='radio' value='fly' name='animation'></input>
+        <span className="radio-dot"></span>                 
+    </label>
+    <label onClick={() => setCardMode('rocket')} className="radio-container label" htmlFor='rocket'>
+        <strong>Rocket Animation</strong>
+        <input id='rocket' type='radio' value='rocket' name='animation'></input>
+        <span className="radio-dot"></span> 
+    </label>
+    {/* the three card view */}
+    <div ref={cardContainerRef} id="cardContainer">
+        <div id={cardMode === 'default' ? 'ellipse-plain' : cardMode === 'rocket' ? 'ellipse-rocket' : 'ellipse'}>
+            {Object.keys(currentRequest).length > 0 && (
+                <>
+                    <h3 id='request-title'>{currentRequest.title}</h3>
+                    <p id='request-desc'><strong>{currentRequest.desc}</strong></p>
+                    <em id='request-status'>
+                    <label className="radio-container" htmlFor={`${currentRequest.status}`}>
+                    <strong>{currentRequest.status}</strong>
+                    <input id={`${currentRequest.status}`} type='radio' value={`${currentRequest.status}`} name='status' disabled></input>
+                    <span className="radio-dot"></span>
+                    </label>
+                    </em>
+                    <small id='creator-sign'>Created By:<br />{currentRequest.creator}</small>
+                    <small id='date-sign'>Created On:<br />{new Date(currentRequest.date).toLocaleDateString()}</small>
+                </>
+            )}
+        </div>
+    </div>
     </>
   ) 
 }
